849bc66eea5ccd3538088ce9d8c779868dd8c54e
retryhipster0v <retry-hipster0v@icloud.com>
2025-10-28 10:15:20 +0100

    Add contracts/ConfidentialETH.sol

---
+++ contracts/ConfidentialETH.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.24;
+
+import {ConfidentialFungibleToken} from "new-confidential-contracts/token/ConfidentialFungibleToken.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
+
+/// @title Confidential ETH (cETH)
+/// @notice Minimal mint-controlled confidential token for faucet distribution
+contract ConfidentialETH is ConfidentialFungibleToken, SepoliaConfig {
+    constructor() ConfidentialFungibleToken("Confidential ETH", "cETH", "") {}
+
+    /// @notice Public mint without encryption to msg.sender
+    function mint(uint64 amount) external {
+        euint64 eamount = FHE.asEuint64(amount);
+        _mint(msg.sender, eamount);
+    }
+
+    /// @notice Public mint with encrypted amount to msg.sender
+    function mintEncrypted(externalEuint64 encryptedAmount, bytes calldata inputProof) external {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        _mint(msg.sender, amount);
+    }
+}
