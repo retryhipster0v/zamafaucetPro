27d7e0048448f3e203170e1dd25e744977c8099c
retryhipster0v <retry-hipster0v@icloud.com>
2025-10-28 10:15:17 +0100

    Add contracts/ConfidentialBTC.sol

---
+++ contracts/ConfidentialBTC.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.24;
+
+import {ConfidentialFungibleToken} from "new-confidential-contracts/token/ConfidentialFungibleToken.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract ConfidentialBTC is ConfidentialFungibleToken, SepoliaConfig {
+    constructor() ConfidentialFungibleToken("Confidential BTC", "cBTC", "") {}
+
+    function mint(uint64 amount) external {
+        euint64 eamount = FHE.asEuint64(amount);
+        _mint(msg.sender, eamount);
+    }
+
+    function mintEncrypted(externalEuint64 encryptedAmount, bytes calldata inputProof) external {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        _mint(msg.sender, amount);
+    }
+}
